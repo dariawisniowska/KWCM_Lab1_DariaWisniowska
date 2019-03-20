b071440d6803cf892de5a018e29d9f5b0e8f13d2
dariawisniowska <dariawisniowska123@gmail.com>
2019-03-20 14:04:09 +0100

    Tests

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,54 @@
 	#include <cmath>
 #endif
 
-int main()
+int main(int argc, char* argv[])
 {
-	double result;
-	#ifdef USE_TRIGONOMETRY_DEGREE
-		std::cout<<"Sinus 45 stopni: ";
-		result = degreemath::sin(45.0);
-	#else
-		std::cout<<"Sinus pi/4 radianow: ";
-		result = std::sin(3.14/4.0);
-	#endif
-
-	std::cout << result <<std::endl;
-
+	double result = 0;
+	if(argc==3){
+		std::string arg = argv[1];
+		if(!arg.compare("--sin"))
+		{			
+			#ifdef USE_TRIGONOMETRY_DEGREE
+				std::cout<<"Sinus dla "<<argv[2] <<" stopni: ";
+				result = degreemath::sin(atof(argv[2]));
+			#else
+				std::cout<<"Sinus "<<argv[2] <<" radianow: ";
+				result = std::sin(atof(argv[2]));
+			#endif		
+		}
+		if(!arg.compare("--cos"))
+		{			
+			#ifdef USE_TRIGONOMETRY_DEGREE
+				std::cout<<"Cosinus dla "<<argv[2] <<" stopni: ";
+				result = degreemath::cos(atof(argv[2]));
+			#else
+				std::cout<<"Cosinus "<<argv[2] <<" radianow: ";
+				result = std::cos(atof(argv[2]));
+			#endif		
+		}
+		if(!arg.compare("--tg"))
+		{			
+			#ifdef USE_TRIGONOMETRY_DEGREE
+				std::cout<<"Tangens dla "<<argv[2] <<" stopni: ";
+				result = degreemath::tg(atof(argv[2]));
+			#else
+				std::cout<<"Tangens "<<argv[2] <<" radianow: ";
+				result = std::tan(atof(argv[2]));
+			#endif		
+		}
+		if(!arg.compare("--ctg"))
+		{			
+			#ifdef USE_TRIGONOMETRY_DEGREE
+				std::cout<<"Kotangens dla "<<argv[2] <<" stopni: ";
+				result = degreemath::ctg(atof(argv[2]));
+			#else
+				std::cout<<"Kotangens "<<argv[2] <<" radianow: ";
+				result = 1/std::tan(atof(argv[2]));
+			#endif		
+		}
+		std::cout << result <<std::endl;
+	}
+	else
+		std::cout << "Nie podano wszystkich parametrow" <<std::endl;
 	return 0;
 }
